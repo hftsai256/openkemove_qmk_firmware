08d01bdbbb1c6708a110db6432413738ba50cfd8
Halley Tsai <hftsai256@gmail.com>
2022-09-14 23:44:25 -0400

    fixup! merge v1.4 into v1.5 as they share the same pin conf

--- keyboards/kemove/boards/KEMOVE_SNOWFOX/v1_5/board.mk
+++ keyboards/kemove/boards/KEMOVE_SNOWFOX/v1_5/board.mk
@@ -1,5 +1,5 @@
 # List of all the board related files.
-BOARDSRC = $(BOARD_PATH)/boards/KEMOVE_SNOWFOX/v1_4/board.c
+BOARDSRC = $(BOARD_PATH)/boards/KEMOVE_SNOWFOX/v1_5/board.c
 
 # Required include directories
-BOARDINC = $(BOARD_PATH)/boards/KEMOVE_SNOWFOX/v1_4
+BOARDINC = $(BOARD_PATH)/boards/KEMOVE_SNOWFOX/v1_5
