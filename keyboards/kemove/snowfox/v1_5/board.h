1d71d1746eef788a542588b1ff203ae593dccf13
Halley Tsai <hftsai256@gmail.com>
2023-04-06 19:52:04 -0400

    add EEPROM pins

--- keyboards/kemove/snowfox/v1_5/board.h
+++ keyboards/kemove/snowfox/v1_5/board.h
@@ -1,7 +1,8 @@
 #pragma once
-#include_next "board.h"
+#include_next <board.h>
 
-#define LINE_DIP0                                       PAL_LINE(IOPORT0, 20)
+#define LINE_DIP0                                       PAL_LINE(IOPORT0, 20) // 3.3v (left) pin
+
 #define LINE_SSP1_MOSI                                  PAL_LINE(IOPORT0, 21)
 #define LINE_SSP1_MISO                                  PAL_LINE(IOPORT1, 21)
 #define LINE_SSP1_SCK                                   PAL_LINE(IOPORT1, 15)
@@ -12,5 +13,10 @@
 #define LINE_UART_TX                                    PAL_LINE(IOPORT1, 27) // To pin 20 on BLE
 #define LINE_BLE_RSTN                                   PAL_LINE(IOPORT0, 23) // To pin 11 on BLE
 
+#define LINE_EEPROM_CS                                  PAL_LINE(IOPORT0, 18)
+#define LINE_EEPROM_DO                                  PAL_LINE(IOPORT0,  8) // MISO0
+#define LINE_EEPROM_DI                                  PAL_LINE(IOPORT0,  9) // MOSI0
+#define LINE_EEPROM_CLK                                 PAL_LINE(IOPORT0, 10) // SCLK0
+#define LINE_EEPROM_HOLD                                PAL_LINE(IOPORT0, 17) // WP
+
 #define LINE_ADC_BAT                                    PAL_LINE(IOPORT0, 15) // To Battery
-                                                                              //
